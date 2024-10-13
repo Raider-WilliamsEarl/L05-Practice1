c764ed8f2453ea36745551069612aa99908d6e0e
Raider Williams-Earl <samwilliamsearl@gmail.com>
2024-10-13 13:14:04 -0600

    first commit

---
+++ js/script.js
@@ -0,0 +1,7 @@
+var fans = document.querySelector(".fans");
+var fish = document.querySelector(".fish");
+var pets = document.querySelector(".pets");
+
+//console.log(fans.outerHTML);
+//console.log(fish.outerHTML);
+//console.log(pets.outerHTML);
