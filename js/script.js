a73ed925f1be0703d3c03c22f0b2af47f9241add
Raider Williams-Earl <samwilliamsearl@gmail.com>
2024-10-13 13:19:30 -0600

    added mouseover events

--- js/script.js
+++ js/script.js
@@ -5,3 +5,13 @@ var pets = document.querySelector(".pets");
 //console.log(fans.outerHTML);
 //console.log(fish.outerHTML);
 //console.log(pets.outerHTML);
+
+fans.addEventListener ("mouseover", function(){
+    fans.innerText ="123K";
+});
+fish.addEventListener ("mouseover", function(){
+    fish.innerText ="47K";
+})
+pets.addEventListener ("mouseover", function(){
+    pets.innerText ="20K";
+})
